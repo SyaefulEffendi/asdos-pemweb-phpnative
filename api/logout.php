7c10bde80c7129ecb49c6532ce1680acf0a4b59b
SyaefulEffendi <mohsyaefuleffendi@gmail.com>
2026-04-21 11:57:05 +0700

    ganti path

--- api/logout.php
+++ api/logout.php
@@ -2,6 +2,6 @@
     session_start();
     session_unset(); // Menghapus semua variabel session
     session_destroy(); // Menghancurkan session
-    header("Location: index.php"); // Kembali ke landing page
+    header("Location: ././index.php"); // Kembali ke landing page
     exit();
 ?>

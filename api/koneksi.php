7f8ac87b028631b5e36446f61153be93088111a5
SyaefulEffendi <mohsyaefuleffendi@gmail.com>
2026-04-22 19:17:56 +0700

    connection

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,12 +1,30 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "asdos-pemweb";
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '4SQbytd1sXK9Aa4.root';
+$pass = 'GSNjampt0pgM6oWJ';
+$db   = 'pemweb-db';
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
 ?>
